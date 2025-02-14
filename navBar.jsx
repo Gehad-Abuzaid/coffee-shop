fc9715fbddde9a7f78b4ecd0cc3d200e050f11e7
Gehad Abdelraziq <gehadabdelrazik9@gmail.com>
2025-02-14 15:32:27 +0200

    Add files via upload

---
+++ navBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navBar = () => {
+  return (
+    <div>
+    <div className="container bg-red-500">
+        <div className="flex">
+            {/*logo section*/}
+            <div>logo</div>
+            {/*links section*/}
+            <div></div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default navBar
